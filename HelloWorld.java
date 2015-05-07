eff20c3824189b17e029554bacce1452b25233b4
unknown <401017852link@gmail.com>
2015-05-07 21:55:15 +0800

    j

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+=======
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }    
+>>>>>>> 34b0606632be1ce130ab86f7149250957d61c909
+}
